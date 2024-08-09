4dbbf2a12d3c775bf080bd4eac95231c3a6b05f7
NdeZzz05 <muhammadfernandes05@gmail.com>
2024-08-10 00:23:19 +0700

    add: data-test

--- routes/api/v1/auth/index.js
+++ routes/api/v1/auth/index.js
@@ -2,16 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 const AUTH_CONTROLLER = require("../../../../controllers/auth.controller");
-const AuthMiddleware = require("../../../../middleware/authentication.middleware");
 const passport = require("passport");
 
-router.get("/register", (req, res) => {
-  res.render("register.ejs", { message: {} });
-});
 router.post("/register", AUTH_CONTROLLER.postRegister);
 
 router.post("/login", AUTH_CONTROLLER.login);
-router.get("/whoami", AuthMiddleware, AUTH_CONTROLLER.whoami);
 
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 router.get("/google/callback", passport.authenticate("google", { failureRedirect: "/auth/redirect", session: false }), AUTH_CONTROLLER.LoginWithGoogle);
